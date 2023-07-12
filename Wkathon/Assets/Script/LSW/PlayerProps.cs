e7531c8adfe5c0586a3308afee363b3f81754185
Hekaline <swlee2282@gmail.com>
2023-07-12 11:07:28 +0900

    Implement store upgrade function
    가게 업그레이드 기능 구현

---
+++ Wkathon/Assets/Script/LSW/PlayerProps.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProps : MonoBehaviour
+{
+    public Int32 money = 0;
+    public Int32 storeLvl = 1;
+}
